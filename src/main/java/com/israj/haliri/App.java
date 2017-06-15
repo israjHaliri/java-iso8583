d85c3733773d674ed74ce8a05038b8a96598e7e0
israjHaliri <israj.haliri@gmail.com>
2017-06-15 14:59:54 +0700

    add source

---
+++ src/main/java/com/israj/haliri/App.java
@@ -0,0 +1,13 @@
+package com.israj.haliri;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
